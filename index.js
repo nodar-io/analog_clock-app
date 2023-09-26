1cfbaddf5f311d69fc7238e92ff551b6209f7184
neidor <tonnymantanno@mail.ru>
2023-09-26 14:01:48 +0300

    1

---
+++ index.js
@@ -0,0 +1,20 @@
+const hr = document.getElementById("hour");
+const min = document.getElementById("min");
+const sec = document.getElementById("sec");
+
+function displayTime() {
+  const date = new Date();
+
+  const hh = date.getHours();
+  const mm = date.getMinutes();
+  const ss = date.getSeconds();
+
+  const hRotation = 30 * hh + mm / 2;
+  const mRotation = 6 * mm;
+  const sRotation = 6 * ss;
+  hr.style.transform = `rotate(${hRotation}deg)`;
+  min.style.transform = `rotate(${mRotation}deg)`;
+  sec.style.transform = `rotate(${sRotation}deg)`;
+}
+
+setInterval(displayTime, 1000);
